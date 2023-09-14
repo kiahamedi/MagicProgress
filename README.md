36416d6d4d73b4291f639e6e2b8a51674cb3a6fb
Kia Hamedi <kia.arta9793@gmail.com>
2023-09-14 05:04:05 +0330

    add project links

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MagicProgress
 A python package that allows to implement and draw various examples of progress bar.
-> version 0.0.2
+> version 0.0.3
 
 [![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity)
 [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Python Magic Progressbar'
 LONG_DESCRIPTION = 'A package that allows to implement and draw various examples of progress bar.'
+
+PROJECT_URLS = {
+  'Homepage': 'https://github.com/kiahamedi/MagicProgress',
+  'Website': 'https://kiahamedi.ir/magic-progress/'
+}
 
 # Setting up
 setup(
@@ -18,6 +23,7 @@ setup(
     author="Kia Hamedi",
     author_email="<kia.arta9793@gmail.com>",
     description=DESCRIPTION,
+    project_urls=PROJECT_URLS,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
